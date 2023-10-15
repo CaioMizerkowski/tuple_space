1f0aa302c44448b6a77893fc568e10cc99b606a7
Caio P. Mizerkowski <caiomizerkowski@gmail.com>
2023-10-14 21:10:12 -0300

    create tuple space

--- src/space.py
+++ src/space.py
@@ -1,2 +1,49 @@
 from typing import NamedTuple
+from random import shuffle
 
+
+class TupleSpace:
+    """Based on:
+    https://en.wikipedia.org/wiki/Tuple_space
+    https://github.com/pSpaces/Programming-with-Spaces/blob/master/tutorial-tuple-spaces.md
+    """
+
+    def __init__(self) -> None:
+        self._space = list()
+
+    def put(self, item: NamedTuple) -> None:
+        self._space.append(item)
+        shuffle(self._space)
+
+    def get(self, template: NamedTuple) -> NamedTuple:
+        # pop the first item that matches the template
+        for item in self._space:
+            if self._match(template, item):
+                self._space.remove(item)
+                return item
+
+    def query(self, template: NamedTuple) -> NamedTuple:
+        # return the first item that matches the template
+        for item in self._space:
+            if self._match(template, item):
+                return item
+
+    def query_all(self, template: NamedTuple) -> NamedTuple:
+        # return all items that match the template
+        items = list()
+        for item in self._space:
+            if self._match(template, item):
+                items.append(item)
+        return items
+
+    def get_all(self, template: NamedTuple) -> NamedTuple:
+        # pop all items that matches the template
+        items = list()
+        for item in self._space:
+            if self._match(template, item):
+                items.append(item)
+                self._space.remove(item)
+
+    def _match(self, template: NamedTuple, item: NamedTuple) -> bool:
+        # check if the template fields match the item fields
+        return template._fields == item._fields
